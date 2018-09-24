33f657636d88c71a01b0d91e62c941ec11f2914b
Andres G. Quina <andresgqjob@gmail.com>
2018-09-24 17:55:36 +0200

    libgdx setup

---
+++ desktop/src/com/andresgq/flyingvirt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.andresgq.flyingvirt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.andresgq.flyingvirt.FlyingVirt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlyingVirt(), config);
+	}
+}
